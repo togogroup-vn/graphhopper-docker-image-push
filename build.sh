3ff1f0848041408ac2beb34f3b700e26414337d0
Ông Thanh Bình <94289551+binhotvn@users.noreply.github.com>
2024-09-28 11:41:46 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -41,7 +41,7 @@ else
   (cd graphhopper; git checkout master; git pull)
 fi
 
-imagename="israelhikingmap/graphhopper:${1:-latest}"
+imagename="binhotvn/graphhopper:${1:-latest}"
 if [ "$1" ]; then
   echo "Checking out graphhopper:$1"
   (cd graphhopper; git checkout --detach "$1")
